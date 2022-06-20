ce17f8e7c2fd6250cbc4b6ba2ef57d2157d145dc
kistyak <kostian38045@gmail.com>
2022-06-20 20:31:43 +0300

    Add new window

--- src/main/java/net/kistyak/registrationwin/SignUpController.java
+++ src/main/java/net/kistyak/registrationwin/SignUpController.java
@@ -3,6 +3,10 @@ package net.kistyak.registrationwin;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class SignUpController {
 
@@ -11,6 +15,30 @@ public class SignUpController {
 
     @FXML
     private URL location;
+
+    @FXML
+    private TextField CountryField;
+
+    @FXML
+    private TextField NameField;
+
+    @FXML
+    private RadioButton RudioButtonFemale;
+
+    @FXML
+    private RadioButton RudioButtonMale;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    private TextField SignUpLoginField;
+
+    @FXML
+    private PasswordField SignUpPasswordField;
+
+    @FXML
+    private TextField SurnameField;
 
     @FXML
     void initialize() {
