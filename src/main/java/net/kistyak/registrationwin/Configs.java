6c78417c42aca1de9b4273d96143a20d3a2fe4e0
kistyak <kostian38045@gmail.com>
2022-06-22 20:29:00 +0300

    null

--- src/main/java/net/kistyak/registrationwin/Configs.java
+++ src/main/java/net/kistyak/registrationwin/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "Kistyak2001";
+    protected String dbPass = "12345";
     protected String dbName = "registration";
 }
